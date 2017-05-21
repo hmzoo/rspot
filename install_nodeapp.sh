319c778fd5f4b9ab3fb98e031636e976579ba926
Your Name <you@example.com>
2017-05-21 20:29:37 +0000

    mosquitto

--- install_nodeapp.sh
+++ install_nodeapp.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 cp -r app /opt/app
+cp nodeapp /etc/init.d
+
 cd /opt/app
 npm install
 npm install -g forever
 
-chown -r pi /opt/app
+chown -R pi /opt/app
 
-cp nodeapp /etc/init.d
 
 chmod 700 /etc/init.d/nodeapp  
 update-rc.d nodeapp defaults
